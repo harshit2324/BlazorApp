0d235655f0ddbd17e027ba3678c30a0afb4e6c41
harshit2324 <richhariyaharshit684@gmail.com>
2025-03-15 09:05:48 +0530

    add: some tasks

---
+++ BlazorApp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<Product_prop> ProductProperties { get; set; }
+    }
+}
---
+++ BlazorApp/Models/Product_prop.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models
+{
+    public class Product_prop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
